7faec9daba546f00e78be59630015a6f7bd8dfb9
Konrad Pilewski <kpilewski@neuca.pl>
2024-05-30 15:55:29 +0200

    Added /author endpoint

--- python/app.py
+++ python/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import random
 import logging
+import os
 
 app = Flask(__name__)
 
@@ -10,6 +11,13 @@ def get_item():
     app.logger.info(f"Random number generated successfully: {number}")
     return str(number)
 
+@app.route('/author', methods=['GET'])
+def author():
+    return "Konrad Pilewski"
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
+    app.logger.info("Starting application version 1.2")
+    app.logger.info(f"Environment: {os.environ}")
+    app.logger.info("Endpoints: /get-item, /author")
     app.run(debug=True, host='0.0.0.0', port=8080)
