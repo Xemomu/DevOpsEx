2f2bf8580c393b55c72a22059ea79ec41721536f
Konrad Pilewski <kpilewski@neuca.pl>
2024-05-31 20:09:52 +0200

    Deleted /loglevels endpoint

--- python/app.py
+++ python/app.py
@@ -1,7 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask
 import random
 import logging
-import os
 
 app = Flask(__name__)
 
@@ -15,22 +14,10 @@ def get_item():
 def author():
     return "Konrad Pilewski"
 
-@app.route('/loglevels', methods=['GET'])
-def loglevels():
-    levels = {
-        "CRITICAL": logging.CRITICAL,
-        "ERROR": logging.ERROR,
-        "WARNING": logging.WARNING,
-        "INFO": logging.INFO,
-        "DEBUG": logging.DEBUG,
-        "NOTSET": logging.NOTSET,
-    }
-    return jsonify(levels)
-
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     app.logger.info("Starting application version 1.2")
     app.logger.info(f"Environment: ")
     app.logger.info("Log levels: CRITICAL, ERROR, WARNING, INFO, DEBUG, NOTSET")
-    app.logger.info("Endpoints: /get-item, /author, /loglevels")
+    app.logger.info("Endpoints: /get-item, /author")
     app.run(debug=True, host='0.0.0.0', port=8080)
